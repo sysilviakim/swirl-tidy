da08e038a3a1feb8d96e21f77d86217cf0a3f169
Seo-young Silvia Kim <sy.silvia.kim@gmail.com>
2020-08-29 00:47:41 -0400

    Import with haven complete

--- Import_with_haven/initLesson.R
+++ Import_with_haven/initLesson.R
@@ -2,3 +2,18 @@
 # lesson is started. Any variables created here will show up in
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
+
+# library(tidyverse)
+# library(haven)
+# df <- read_dta("cces18_common_vv.dta")
+# set.seed(123)
+# cces18 <- df %>% 
+#   sample_n(50) %>% 
+#   select(inputstate, birthyr, gender, educ, race, pid3)
+
+lesson_dir <- file.path(
+  path.package(substring(find("swirl")[1], 9)), "Courses",
+  "swirl-tidy", "Import_with_haven"
+)
+
+vec_nolabel <- c(1, 1, 3, 2, 4, 4)
