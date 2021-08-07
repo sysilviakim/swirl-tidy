d7e7c91515f16f2029360e3e5b99b8a71a13e348
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-08-07 13:43:28 -0400

    readxl complete (must deal with lesson_dir someday)

--- Import_with_readxl/initLesson.R
+++ Import_with_readxl/initLesson.R
@@ -2,3 +2,8 @@
 # lesson is started. Any variables created here will show up in
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
+
+lesson_dir <- file.path(
+  path.package(substring(find("swirl")[1], 9)), "Courses",
+  "swirl-tidy", "Import_with_readxl"
+)
